0c396b963d6821bcb6193363494bd13f3aebd1db
Aaro Saila <aaros.saila@gmail.com>
2023-11-07 16:21:09 +0200

    asdsd

--- Ohjelmisto_2/tehtava_2/tehtava2_1.py
+++ Ohjelmisto_2/tehtava_2/tehtava2_1.py
@@ -1,9 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
 import matplotlib as mpl
 
-fig, ax = plt.subplots()
-ax.plot([1, 2, 3, 4], [1, 2, 3, 4])
+fig = plt.figure()
+ax = fig.subplots()
 
-plt.show()
+ymp = plt.Circle((0, 0), radius=1, fill=0)
+ax.add_patch(ymp)
+
+ax.spines['left'].set_position('center')
+ax.spines['bottom'].set_position('center')
+
+ax.axis('equal')
+
+plt.xticks([-1, 0, 1], minor=True)
+plt.yticks([-1, 0, 1])
+
+pist_xy = np.array
